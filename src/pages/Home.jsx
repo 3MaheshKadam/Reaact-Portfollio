50c1f6b95aae10e2ec40586fdc95e1d1d84e0647
3MaheshKadam <maheshkadam9298@gmail.com>
2024-07-30 20:19:35 +0530

    home ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import profilePic from "../assets/profile/profile/developer-pic-1.png";
+import AnimatedText from "../components/AnimatedText";
+import dummy from "../assets/dummy.pdf"; // Ensure the path to the PDF is correct
+import { LinkArrow } from "../components/Icons.jsx";
 const Home = () => {
   return (
     <main className="flex items-center text-dark w-full min-h-screen">
-      .
-      <layout className="p-0">
+      <div className="layout p-0">
         <div className="flex items-center justify-between w-full">
           <div className="w-1/2">
-            {/* //w-1/2 */}
-            <img src={profilePic} alt="Mahesh" className="w-full   px-20" />
+            <img src={profilePic} alt="Mahesh" className="w-full px-20" />
           </div>
-          <div className="w-1/2">
-            <h1>IIt will work one day and i'll make it</h1>
-            <p className="">
-              {" "}
+          <div className="w-1/2 flex flex-col items-center self-center">
+            <AnimatedText
+              text={"It will work one day and I'll make it!"}
+              className="!text-left"
+            />
+            <p className="my-4 text-base font-medium">
               As a skilled full-stack developer, I am dedicated to turning ideas
               into innovative web applications. Explore my latest projects and
-              articles, showcasing my expertise in React.js and web development.{" "}
+              articles, showcasing my expertise in React.js and web development.
             </p>
+            <div className="flex items-center self-start mt-2">
+              <a
+                href={dummy}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                download
+              >
+                Resume
+                <LinkArrow className={"w-6 ml-1"} />
+              </a>
+              <a
+                href="mailto:maheshkadam9298@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-4 text-lg font-semibold text-dark hover:underline"
+              >
+                Contact
+              </a>
+            </div>
           </div>
         </div>
-      </layout>
+      </div>
     </main>
   );
 };
