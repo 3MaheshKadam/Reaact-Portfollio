5175374c2e96c393f2d08e0e2d65a87fd1a0e99f
3MaheshKadam <maheshkadam9298@gmail.com>
2024-08-06 17:55:28 +0530

    base case navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -49,58 +49,56 @@ const NavBar = () => {
   }, [isMenuOpen]);
 
   return (
-    <header className="w-full px-4 py-4 font-medium flex flex-wrap items-center justify-between bg-gray-800 shadow-md relative">
-      {/* Logo */}
-      <div className="flex items-center justify-between w-full sm:w-auto">
-        <Logo />
-        <button
-          className="block sm:hidden px-2 py-1 text-white"
-          onClick={toggleMenu}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+    <header className="w-full px-4 py-4 font-medium flex items-center justify-between bg-gray-800 shadow-md relative">
+      <div className="w-full flex items-center justify-between">
+        {/* Logo and Menu Toggle */}
+        <div className="flex items-center justify-between w-full sm:w-auto">
+          <Logo />
+          <button
+            className="block sm:hidden px-2 py-1 text-white ml-4"
+            onClick={toggleMenu}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Centered Navigation */}
+        <div className="hidden sm:flex flex-grow items-center justify-center">
+          <nav className="flex space-x-4">
+            <CustomLink to="/" title="Home" className="py-2 px-4 text-white" />
+            <CustomLink
+              to="/about"
+              title="About"
+              className="py-2 px-4 text-white"
             />
-          </svg>
-        </button>
-      </div>
+            <CustomLink
+              to="/projects"
+              title="Projects"
+              className="py-2 px-4 text-white"
+            />
+            <CustomLink
+              to="/experience"
+              title="Experience"
+              className="py-2 px-4 text-white"
+            />
+          </nav>
+        </div>
 
-      {/* Mobile Menu */}
-      <div
-        ref={menuRef}
-        className={`${
-          isMenuOpen ? "block" : "hidden"
-        } sm:hidden absolute top-full left-0 w-full bg-white shadow-md flex flex-col items-center py-4 z-10`}
-      >
-        <nav className="flex flex-col items-center w-full">
-          <CustomLink to="/" title="Home" className="py-2 px-4 text-white" />
-          <CustomLink
-            to="/about"
-            title="About"
-            className="py-2 px-4 text-white"
-          />
-          <CustomLink
-            to="/projects"
-            title="Projects"
-            className="py-2 px-4 text-white"
-          />
-          <CustomLink
-            to="/experience"
-            title="Experience"
-            className="py-2 px-4 text-white"
-          />
-        </nav>
-
-        <nav className="flex items-center justify-center gap-4 mt-4">
+        {/* Social Icons */}
+        <nav className="hidden sm:flex items-center gap-4">
           <motion.a
             href="https://github.com/3maheshkadam"
             target="_blank"
@@ -140,65 +138,71 @@ const NavBar = () => {
         </nav>
       </div>
 
-      {/* Desktop Menu */}
-      <nav className="hidden sm:flex sm:items-center sm:space-x-4">
-        <CustomLink to="/" title="Home" className="py-2 px-4 text-white" />
-        <CustomLink
-          to="/about"
-          title="About"
-          className="py-2 px-4 text-white"
-        />
-        <CustomLink
-          to="/projects"
-          title="Projects"
-          className="py-2 px-4 text-white"
-        />
-        <CustomLink
-          to="/experience"
-          title="Experience"
-          className="py-2 px-4 text-white"
-        />
-      </nav>
+      {/* Mobile Menu */}
+      <div
+        ref={menuRef}
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } sm:hidden absolute top-full left-0 w-full bg-white shadow-md flex flex-col items-center py-4 z-10`}
+      >
+        <nav className="flex flex-col items-center w-full">
+          <CustomLink to="/" title="Home" className="py-2 px-4 text-black" />
+          <CustomLink
+            to="/about"
+            title="About"
+            className="py-2 px-4 text-black"
+          />
+          <CustomLink
+            to="/projects"
+            title="Projects"
+            className="py-2 px-4 text-black"
+          />
+          <CustomLink
+            to="/experience"
+            title="Experience"
+            className="py-2 px-4 text-black"
+          />
+        </nav>
 
-      {/* Social icons (Desktop) */}
-      <nav className="hidden sm:flex items-center justify-between gap-4">
-        <motion.a
-          href="https://github.com/3maheshkadam"
-          target="_blank"
-          className="w-6 text-white"
-          rel="noopener noreferrer"
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"
-          variants={iconVariants}
-        >
-          <GithubIcon />
-        </motion.a>
-        <motion.a
-          href="https://linkedin.com/in/mahesh-kadam-6a9b13219"
-          target="_blank"
-          className="w-6 text-gray-700"
-          rel="noopener noreferrer"
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"
-          variants={iconVariants}
-        >
-          <LinkedInIcon />
-        </motion.a>
-        <motion.a
-          href="https://x.com/maheshkadam2524"
-          target="_blank"
-          className="w-6 text-gray-700"
-          rel="noopener noreferrer"
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"
-          variants={iconVariants}
-        >
-          <TwitterIcon />
-        </motion.a>
-      </nav>
+        <nav className="flex items-center justify-center gap-4 mt-4">
+          <motion.a
+            href="https://github.com/3maheshkadam"
+            target="_blank"
+            className="w-6 text-black"
+            rel="noopener noreferrer"
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"
+            variants={iconVariants}
+          >
+            <GithubIcon />
+          </motion.a>
+          <motion.a
+            href="https://linkedin.com/in/mahesh-kadam-6a9b13219"
+            target="_blank"
+            className="w-6 text-black"
+            rel="noopener noreferrer"
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"
+            variants={iconVariants}
+          >
+            <LinkedInIcon />
+          </motion.a>
+          <motion.a
+            href="https://x.com/maheshkadam2524"
+            target="_blank"
+            className="w-6 text-black"
+            rel="noopener noreferrer"
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"
+            variants={iconVariants}
+          >
+            <TwitterIcon />
+          </motion.a>
+        </nav>
+      </div>
     </header>
   );
 };
