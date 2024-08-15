41790caf04a0a7cc121434069e9767e4f0fbf132
3MaheshKadam <maheshkadam9298@gmail.com>
2024-08-15 21:37:10 +0530

    services in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,11 +14,11 @@ const About = () => {
           content="A passionate web developer specializing in dynamic applications and modern technologies."
         />
       </Helmet>
-      <main className="flex w-full flex-col items-center justify-center bg-light dark:bg-dark text-dark dark:text-light">
+      <main className="flex w-full flex-col items-center justify-center bg-light dark:bg-dark text-dark dark:text-light ">
         <div className="p-4 sm:p-8">
           <AnimatedText
             text="Solutions with Impact !"
-            className="sm:text-3xl md:text-4xl !text-6xl"
+            className="sm:text-3xl md:text-4xl !text-6xl sm:mb-5"
           />
           <div className="mt-10 grid w-full grid-cols-1 md:grid-cols-8 lg:grid-cols-6 gap-8 md:gap-16">
             <div className="md:col-span-6 lg:col-span-4 bg-gray-300 dark:bg-gray-800 flex flex-col items-start justify-start py-4 sm:py-8">
@@ -58,7 +58,7 @@ const About = () => {
               />
             </div>
           </div>
-          {/* New Section for Large Screens */}
+          {/* Professional Statistics Section */}
           <div className="flex flex-col items-center justify-center mt-10">
             <AnimatedText
               className="!text-4xl font-bold mb-6"
@@ -76,6 +76,69 @@ const About = () => {
               <div className="flex flex-col items-center">
                 <div className="text-3xl font-semibold">2</div>
                 <div className="text-lg font-semibold">Years of Experience</div>
+              </div>
+            </div>
+          </div>
+          {/* Services Section */}
+          <div className="mt-10 w-full bg-gray-300 dark:bg-gray-800 p-4 sm:p-8">
+            <AnimatedText
+              text="Services I Offer"
+              className="!text-3xl font-bold mb-6"
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              <div className="flex flex-col items-center bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl">
+                <h3 className="text-xl font-semibold mb-3">Web Development</h3>
+                <p className="text-base text-center text-gray-700 dark:text-gray-300">
+                  Building responsive and high-performance web applications
+                  using the latest technologies including React.js, Next.js, and
+                  more.
+                </p>
+              </div>
+              <div className="flex flex-col items-center bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl">
+                <h3 className="text-xl font-semibold mb-3">
+                  Backend Development
+                </h3>
+                <p className="text-base text-center text-gray-700 dark:text-gray-300">
+                  Developing robust server-side applications and APIs using
+                  Node.js, Express.js, and MongoDB.
+                </p>
+              </div>
+              <div className="flex flex-col items-center bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl">
+                <h3 className="text-xl font-semibold mb-3">
+                  Custom Software Creation
+                </h3>
+                <p className="text-base text-center text-gray-700 dark:text-gray-300">
+                  Develop custom software tailored to your specific needs,
+                  whether it's an online store or a social blogging site.
+                </p>
+              </div>
+              <div className="flex flex-col items-center bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl">
+                <h3 className="text-xl font-semibold mb-3">API Building</h3>
+                <p className="text-base text-center text-gray-700 dark:text-gray-300">
+                  Create and design APIs that allow different parts of your app,
+                  or even different apps, to talk to each other smoothly and
+                  efficiently.
+                </p>
+              </div>
+              <div className="flex flex-col items-center bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl">
+                <h3 className="text-xl font-semibold mb-3">
+                  Deployment and Support
+                </h3>
+                <p className="text-base text-center text-gray-700 dark:text-gray-300">
+                  Help launch your MERN stack or Next.js applications into the
+                  real world, and provide ongoing support to keep them running
+                  smoothly, securely, and up-to-date.
+                </p>
+              </div>
+              <div className="flex flex-col items-center bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl">
+                <h3 className="text-xl font-semibold mb-3">
+                  Consulting and Training
+                </h3>
+                <p className="text-base text-center text-gray-700 dark:text-gray-300">
+                  Offer advice and guidance for businesses looking to use the
+                  MERN stack or improve their current applications, and provide
+                  training to help teams learn and grow.
+                </p>
               </div>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import profilePic from "../assets/profile/profile/developer-pic-1.png";
 import AnimatedText from "../components/AnimatedText";
-import dummy from "../assets/dummy.pdf"; // Ensure the path to the PDF is correct
+import dummy from "../assets/dummy.pdf";
 import { LinkArrow } from "../components/Icons.jsx";
 import HireMe from "../components/HireMe.jsx";
 import lightBulb from "../assets/profile/svgs/miscellaneous_icons_1.svg";
@@ -14,7 +14,7 @@ import next from "../assets/profile/profile/nextjs.png";
 
 const Home = () => {
   return (
-    <main className="flex items-center text-dark dark:text-light w-full min-h-screen bg-light dark:bg-dark">
+    <main className="flex items-center text-dark dark:text-light w-full min-h-screen bg-light dark:bg-dark sm:px-2">
       <div className="absolute left-2 flex top-20 pt-4 px-2 gap-2">
         <img src={mongo} alt="MongoDB" className="w-12 h-12 mb-2" />
         <img src={express} alt="Express" className="w-12 h-12 mb-2" />
@@ -22,10 +22,9 @@ const Home = () => {
         <img src={node} alt="Node.js" className="w-12 h-12 mb-2" />
         <img src={next} alt="Next.js" className="w-12 h-12 mb-2" />
       </div>
-      <div className="layout p-0">
+      <div className="layout p-0 mt-14">
         <div className="flex flex-col md:flex-row items-center justify-between w-full bg-slate-300 dark:bg-slate-800">
           <div className="w-full md:w-1/2 flex justify-center">
-            {/* Only display the image on md screens and larger */}
             <img
               src={profilePic}
               alt="Mahesh"
@@ -66,7 +65,7 @@ const Home = () => {
         </div>
       </div>
       <HireMe />
-      <div className="absolute sm:z-0 lg:z-10 right-6 top-20 inline-block w-24">
+      <div className="absolute sm:z-0 lg:z-10 right-6 top-20 inline-block w-24 ">
         <img src={lightBulb} alt="Filament" className="w-full h-auto" />
       </div>
     </main>
