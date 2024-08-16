a214b14c2d7e0c536fdf618732ae685a2b5a8427
3MaheshKadam <maheshkadam9298@gmail.com>
2024-08-16 13:14:07 +0530

    project section update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion"; // Import Framer Motion
-import ecom from "../assets/profile/projects/ecom.jpg";
+import { motion } from "framer-motion";
+import ecom from "../assets/profile/projects/ecom.png";
 import estate from "../assets/profile/projects/estate.png";
 import movies from "../assets/profile/projects/movies.png";
 import notes from "../assets/profile/projects/notes.png";
-import weather from "../assets/profile/projects/weather.jpg";
-import blog from "../assets/profile/projects/blog.jpg";
+import weather from "../assets/profile/projects/weather.png";
+import blog from "../assets/profile/projects/blog.png";
 
 const projects = [
   {
@@ -26,14 +26,14 @@ const projects = [
     image: estate,
     link: "https://mern-estate-16o5.onrender.com/",
   },
-  { id: 4, title: "Notes App", image: notes, link: "#" }, // Replace # with the actual link when available
+  { id: 4, title: "Notes App", image: notes, link: "#" },
   {
     id: 5,
     title: "E-commerce",
     image: ecom,
     link: "https://ecommerce-mern-eight-orcin.vercel.app/",
   },
-  { id: 6, title: "Weather App", image: weather, link: "#" }, // Replace # with the actual link when available
+  { id: 6, title: "Weather App", image: weather, link: "#" },
 ];
 
 const Projects = () => {
@@ -49,24 +49,24 @@ const Projects = () => {
         </h2>
         <motion.div
           className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
-          initial={{ opacity: 0, y: 50 }} // Initial state: hidden and below the view
-          animate={{ opacity: 1, y: 0 }} // Final state: visible and at its original position
-          transition={{ duration: 0.8, ease: "easeOut" }} // Animation duration and easing
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
         >
           {projects.map((project) => (
             <motion.div
               key={project.id}
               className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden cursor-pointer"
               onClick={() => handleClick(project.link)}
-              whileHover={{ scale: 1.05 }} // Scale up slightly on hover
-              whileTap={{ scale: 0.95 }} // Scale down slightly on tap/click
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
             >
               <motion.img
                 src={project.image}
                 alt={project.title}
-                className="w-full h-48 object-cover"
-                whileHover={{ scale: 1.1 }} // Zoom in image slightly on hover
-                transition={{ duration: 0.3 }} // Smooth transition for the hover effect
+                className="w-full h-40 object-contain" // Adjusted height and object-fit
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.3 }}
               />
               <div className="p-4">
                 <h3 className="text-xl font-semibold text-dark dark:text-light text-center">
